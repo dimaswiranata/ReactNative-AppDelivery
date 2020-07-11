44cb936eeb6003876dfe540a6af1bcdaa7ec569c
Dimas Agusta Wiranata <dimaswiranata8@gmail.com>
2020-07-11 17:39:26 +0700

    fix product counter and ignore warning

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, YellowBox } from 'react-native'
 import Navigation from "./src/navigation";
 
 const App = () => {
+  YellowBox.ignoreWarnings(['VirtualizedLists should never be nested inside plain ScrollViews with the same orientation - use another VirtualizedList-backed container instead']);
   return <Navigation/>;
 }
 
